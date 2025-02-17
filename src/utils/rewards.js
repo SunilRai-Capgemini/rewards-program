3d18b0c9bfea288ec802ab8b7db5aa26310c0edf
Rai <sunil.rai@capgemini.com>
2025-02-17 22:14:16 +0530

    Refactor reward points calculation to simplify logic without using Math.min.

--- src/utils/rewards.js
+++ src/utils/rewards.js
@@ -1,19 +1,19 @@
-import { useCallback } from 'react';
+import { useCallback } from "react";
 
 export const calculateRewardsPoints = (amount) => {
   let points = 0;
-  
+
   // Calculate 2 points for every dollar spent over $100
   if (amount > 100) {
     points += (amount - 100) * 2;
   }
-  
+
   // Calculate 1 point for every dollar spent between $50-$100
   if (amount > 50) {
-    const dollars = Math.min(amount, 100) - 50;
+    const dollars = amount > 100 ? 50 : amount - 50;
     points += dollars * 1;
   }
-  
+
   return points;
 };
 
@@ -26,7 +26,7 @@ export const useCalculateCustomerTotalRewardsPoints = () => {
       });
       const points = calculateRewardsPoints(transaction.amount);
       if (!rewardsPoints[transaction.customerName]) {
-        rewardsPoints[transaction.customerName] = {monthly: {}, total: 0};
+        rewardsPoints[transaction.customerName] = { monthly: {}, total: 0 };
       }
       if (!rewardsPoints[transaction.customerName].monthly[month]) {
         rewardsPoints[transaction.customerName].monthly[month] = 0;
